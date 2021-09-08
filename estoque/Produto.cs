71153dc109cfbd7a68e054570ac93a55ee55068b
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-09-08 19:29:16 -0300

    Add project files.

---
+++ estoque/Produto.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace estoque
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quant { get; set; }
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quant)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quant = quant;
+        }
+        public double valorTotalEmEstoque()
+        {
+            return Preco * Quant;
+        }
+        public void adcionarProdutos(int quantidade)
+        {
+            Quant += quantidade;
+        }
+        public void removerProdutos(int quantidade)
+        {
+            Quant -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", R$" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quant + " Unidades, Total: " + (Preco * Quant).ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ estoque/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com os produtos");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade em estoque: ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Produto prod = new Produto(nome, preco, quant);
+
+            Console.WriteLine("Dados do produto: " + prod);
+
+            Console.Write("Digite o número a ser adicionado no estoque: ");
+            int ad = int.Parse(Console.ReadLine());
+            prod.adcionarProdutos(ad);
+
+            Console.WriteLine("Dados atualizados: " + prod);
+
+
+
+        }
+    }
+}
